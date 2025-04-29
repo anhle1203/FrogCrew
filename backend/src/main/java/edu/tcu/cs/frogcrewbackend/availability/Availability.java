df5ce0fcde60f14a18f8b7de7c2498ddbe4134de
anhle1203 <125301207+anhle1203@users.noreply.github.com>
2025-04-28 23:47:53 -0500

    Chore: Push all, fix typos

---
+++ backend/src/main/java/edu/tcu/cs/frogcrewbackend/availability/Availability.java
@@ -0,0 +1,67 @@
+package edu.tcu.cs.frogcrewbackend.availability;
+
+import edu.tcu.cs.frogcrewbackend.game.Game;
+import edu.tcu.cs.frogcrewbackend.member.Member;
+import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
+
+import java.io.Serializable;
+
+@Transactional
+@Entity
+public class Availability implements Serializable {
+    // Each user submits 1 availability
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    private Member user;
+
+    @ManyToOne
+    private Game game;
+
+    private Boolean availability;
+
+    private String comment;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Member getUser() {
+        return user;
+    }
+
+    public void setUser(Member user) {
+        this.user = user;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
